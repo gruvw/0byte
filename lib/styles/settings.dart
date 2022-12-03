b116948ac043168fedf4d340b8bb5924f014ddc2
gruvw <gruvw.dev@gmail.com>
2022-12-03 22:40:14 +0100

    Gitignore

--- lib/styles/settings.dart
+++ lib/styles/settings.dart
@@ -2,5 +2,5 @@ class SettingsTheme {
   static const double slidableExtendRatio = 0.2;
   static const double formDialogWidthRatio = 0.7;
   static const double floatingActionChildrenSize = 62;
-  static const double conversionSpacing = 24;
+  static const double conversionSpacing = 20;
 }
